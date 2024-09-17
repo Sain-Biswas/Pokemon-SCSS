6514f9072ff3c1b4685a31afc07882fd826b6499
Sain-Biswas <sain.biswas614@gmail.com>
2024-09-18 01:20:12 +0530

    Initial Commit

---
+++ src/database/db.ts
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+type ConnectionObject = {
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+async function dbConnect(): Promise<void> {
+  if (connection.isConnected) {
+    console.log("Already connected to database");
+    return;
+  }
+
+  try {
+    const db = await mongoose.connect(process.env.MONGODB_URI as string);
+
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log("Database connection successfull");
+  } catch (error: any) {
+    console.log("Database connection failed : ", error);
+  }
+}
+
+export default dbConnect;
